a613d6622b2dc5d41b0de5de50a7d8b376aaac51
Mateusz Tarnawa <mtarnawa@openmailbox.org>
2017-08-05 09:51:29 +0200

    Some refactoring, requests module added, retries added, filesaving from stream (chunked)

--- uidextract.py
+++ uidextract.py
@@ -3,48 +3,61 @@
 import sys
 import os
 import json
-from urllib import request
+import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
-if len(sys.argv) != 6:
-	print("Syntax: ./uidextract.py <csrftoken> <sessionid> <mailbox> <lowerbound> <upperbound>")
-	exit()
 
-csrftoken = sys.argv[1]
-sessionid = sys.argv[2]
-mailbox = sys.argv[3]
-lowerbound = int(sys.argv[4])
-upperbound = int(sys.argv[5])
+def get_emails(csrftoken, sessionid, mailbox, lowerbound, upperbound):
+    print("Getting list of emails")
+    # Create a session object from requests library
+    s = requests.Session()
+    retries = Retry(total=10, backoff_factor=1,
+                    status_forcelist=[500, 502, 504])
+    s.mount('https://', HTTPAdapter(max_retries=retries))
+    s.headers.update({'Cookie': 'csrftoken={0};'
+                      'sessionid={1}'.format(csrftoken, sessionid)})
+    mdatareq = 'https://app.openmailbox.org/requests/webmail?range={0}-{1}&sort=date&order=0&selected=&action=maillist&mailbox={2}'.format(lowerbound, upperbound, mailbox)
+    print(mdatareq)
 
-if upperbound <= lowerbound:
-	print("The lower bound must be less than than the upper bound")
-	exit()
+    metadata = json.loads(s.get(mdatareq).text)
+    print(metadata)
+    uids = []
+    for line in metadata['partial_list']:
+        uids.append(line['uid'])
+    print("Finished getting list of emails")
 
-if upperbound - lowerbound > 500:
-	print("The difference between the upper bound and the lower bound must be less than or equal to 500.")
-	exit()
+    os.makedirs('emails_output_dir', exist_ok=True)
+    print("Created directory emails_output_dir if it didn't already exist")
 
-print("Getting list of emails")
-mdatareq = request.Request('https://app.openmailbox.org/requests/webmail?range='+str(lowerbound)+'-'+str(upperbound)+'&sort=date&order=0&selected=&action=maillist&mailbox='+mailbox)
-mdatareq.add_header('Cookie', 'csrftoken='+csrftoken+'; sessionid='+sessionid)
-mdataresp = request.urlopen(mdatareq)
-metadata = json.loads(mdataresp.read().decode('utf-8'))
-uids = []
-for line in metadata['partial_list']:
-	uids.append(line['uid'])
-print("Finished getting list of emails")
+    for uid in uids:
+        if not os.path.isfile('emails_output_dir/' + str(uid) + ".eml"):
+            req = 'https://app.openmailbox.org/requests/webmail?mailbox={0}&uid={1}&action=downloadmessage'.format(mailbox, str(uid))
+            resp = s.get(req, stream=True)
+            with open('emails_output_dir/' + str(uid) + '.eml', 'wb') as eml:
+                for chunk in resp:
+                    eml.write(chunk)
+            print("Saved message " + str(uid))
+        else:
+            print("Already downloaded " + str(uid))
 
-os.makedirs('emails_output_dir', exist_ok=True)
-print("Created directory emails_output_dir if it didn't already exist")
 
-for uid in uids:
-	if not os.path.isfile('emails_output_dir/'+str(uid)+".eml"):
-		req = request.Request('https://app.openmailbox.org/requests/webmail?mailbox='+mailbox+'&uid='+str(uid)+'&action=downloadmessage')
-		req.add_header('Cookie', 'csrftoken='+csrftoken+'; sessionid='+sessionid)
-		resp = request.urlopen(req)
+if __name__ == '__main__':
+    csrftoken = sys.argv[1]
+    sessionid = sys.argv[2]
+    mailbox = sys.argv[3]
+    lowerbound = sys.argv[4]
+    upperbound = sys.argv[5]
 
-		output = open("emails_output_dir/"+str(uid)+".eml",'w')
-		output.write(resp.read().decode('utf-8'))
-		output.close()
-		print("Saved message " + str(uid))
-	else:
-		print("Already downloaded " + str(uid))
+    if int(upperbound) <= int(lowerbound):
+        print("The lower bound must be less than than the upper bound")
+        exit()
+    if int(upperbound) - int(lowerbound) > 500:
+        print('The difference between the upper bound and the lower'
+              'bound must be less than or equal to 500.')
+        exit()
+    if len(sys.argv) != 6:
+        print('Syntax: ./uidextract.py <csrftoken> <sessionid>'
+              '<mailbox> <lowerbound> <upperbound>')
+        exit()
+    get_emails(csrftoken, sessionid, mailbox, lowerbound, upperbound)
